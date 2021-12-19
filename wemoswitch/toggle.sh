17aea59bed688604267f1ea869bf39d50ea24d98
Allen Rawls <rawslaw@users.noreply.github.com>
2021-12-19 14:29:07 -0500

    add those shebangs

--- wemoswitch/toggle.sh
+++ wemoswitch/toggle.sh
@@ -1,4 +1,5 @@
-STATE=`curl -s -H 'Content-type:text/xml;  charset=utf-8' -H 'SOAPACTION:"urn:Belkin:service:basicevent:1#GetBinaryState"' -d '<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"> <s:Body><u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"></u:GetBinaryState></s:Body></s:Envelope>' "http://$1:49153/upnp/control/basicevent1" | xpath -e "/s:Envelope/s:Body/u:GetBinaryStateResponse/BinaryState/text()"`
+#!/bin/bash
+STATE=`curl -s -H 'Content-type:text/xml;  charset=utf-8' -H 'SOAPACTION:"urn:Belkin:service:basicevent:1#GetBinaryState"' -d '<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"> <s:Body><u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"></u:GetBinaryState></s:Body></s:Envelope>' "http://$1:49153/upnp/control/basicevent1" | xpath -q -e "/s:Envelope/s:Body/u:GetBinaryStateResponse/BinaryState/text()"`
 
 if [ "$STATE" == "0" ]; then
   echo "$1 is off, turning on";
